16d31fab894fd218a251ad46fdb091fe812f9f26
ferrucc-io <hi@ferrucc.io>
2018-11-02 20:21:08 +0100

    React

---
+++ public/src/components/Profile.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import {
+  isSignInPending,
+  loadUserData,
+  Person,
+} from 'blockstack';
+
+const avatarFallbackImage = 'https://s3.amazonaws.com/onename/avatar-placeholder.png';
+
+export default class Profile extends Component {
+  constructor(props) {
+  	super(props);
+
+  	this.state = {
+  	  person: {
+  	  	name() {
+          return 'Anonymous';
+        },
+  	  	avatarUrl() {
+  	  	  return avatarFallbackImage;
+  	  	},
+  	  },
+  	};
+  }
+
+  render() {
+    const { handleSignOut } = this.props;
+    const { person } = this.state;
+    return (
+      !isSignInPending() ?
+      <div className="panel-welcome" id="section-2">
+        <div className="avatar-section">
+          <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } className="img-rounded avatar" id="avatar-image" />
+        </div>
+        <h1>Hello, <span id="heading-name">{ person.name() ? person.name() : 'Nameless Person' }</span>!</h1>
+        <p className="lead">
+          <button
+            className="btn btn-primary btn-lg"
+            id="signout-button"
+            onClick={ handleSignOut.bind(this) }
+          >
+            Logout
+          </button>
+        </p>
+      </div> : null
+    );
+  }
+
+  componentWillMount() {
+    this.setState({
+      person: new Person(loadUserData().profile),
+    });
+  }
+}
