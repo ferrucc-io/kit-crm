d323d905d4c43ae4e3f598f8d05f80387b1d1325
ferrucc-io <hi@ferrucc.io>
2018-11-04 17:01:14 +0100

    Image Responsiveness

--- public/src/components/SingleContact.js
+++ public/src/components/SingleContact.js
@@ -8,8 +8,8 @@ export default class SingleContact extends Component {
         <div className="w-20 w-10-ns">
           <img
             src={`https://avatars.io/twitter/${contact.twitterHandle}`}
-            className="fl br-100 w3"
-            alt="Profile Image"
+            className="fl br-100 w3 mt2-m mt0-l w-100 w-70-l"
+            alt=""
           />
         </div>
         <p className="fl w-80 w-90-ns h3 pl3">
