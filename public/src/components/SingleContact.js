f7f11bf986d5f9ce68f7091999de02c78c55883a
ferrucc-io <hi@ferrucc.io>
2018-11-04 16:37:34 +0100

    Display twitter profile image

--- public/src/components/SingleContact.js
+++ public/src/components/SingleContact.js
@@ -3,6 +3,17 @@ import React, { Component } from 'react';
 export default class SingleContact extends Component {
   render() {
     const { contact } = this.props;
-    return <div>{contact.name}</div>;
+    return (
+      <div className="db">
+        <img
+          src={`https://avatars.io/twitter/${contact.twitterHandle}`}
+          className="fl br-100 h3 w3 w-10"
+          alt="Profile Image"
+        />
+        <p className="fl w-90 h3 pl3">
+          {contact.name} {contact.lastName}
+        </p>
+      </div>
+    );
   }
 }
