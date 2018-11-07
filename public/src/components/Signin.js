55a47271fac9b55ac2d0eb3be6c2fe1ef395d93b
ferrucc-io <hi@ferrucc.io>
2018-11-07 22:03:10 +0100

    Home Page Styling

--- public/src/components/Signin.js
+++ public/src/components/Signin.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import { createGlobalStyle } from 'styled-components';
 import KitLogo from '../assets/icon-logo.png';
+import BlockstackLogo from '../assets/blockstack-logo-landscape@2x.png';
+import DefaultButton from './styles/DefaultButton';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-image: radial-gradient(circle, #D7D7D7, #D7D7D7 1px, #FFF 1px, #FFF);
+    background-size: 28px 28px;
+  }
+`;
 export default class SignIn extends Component {
   constructor(props) {
     super(props);
@@ -10,21 +19,25 @@ export default class SignIn extends Component {
     const { handleSignIn } = this.props;
 
     return (
-      <div className="panel-landing" id="section-1">
-        <div className="mb4 mt4">
+      <div className="landing">
+        <div className="mb4 pt4">
           <img src={KitLogo} alt="Kit Logo" />
           <h1 className="f1 mb0">KIT CRM</h1>
           <h2 className="mt0 f4">Keep In Touch, your personal CRM</h2>
         </div>
         <p>
-          <button
+          <DefaultButton
             className="f6 link dim ph3 pv2 mb2 dib white bg-black b--black"
             id="signin-button"
             onClick={handleSignIn.bind(this)}
           >
-            Sign In with Blockstack
-          </button>
+            Sign In
+          </DefaultButton>
+          <DefaultButton className="f6 link dim ph3 pv2 mb2 dib gray bg-white b--gray ml2">
+            <div className="dib">Learn More →</div>
+          </DefaultButton>
         </p>
+        <GlobalStyle />
       </div>
     );
   }
---
+++ public/src/components/styles/DefaultButton.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const DefaultButton = styled.button`
+  text-transform: uppercase;
+  font-size: 12px;
+  border-radius: 5px;
+  user-select: none;
+  align-items: center;
+  font-weight: 200;
+  width: 130px;
+  align-items: center;
+  -webkit-box-pack: center;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  height: 40px;
+`;
+
+export default DefaultButton;
