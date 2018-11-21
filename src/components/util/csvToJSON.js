27352d31f0472ade5e61c297ceb8ee9237e15b5b
ferrucc-io <hi@ferrucc.io>
2018-11-21 16:49:42 +0100

    Write Importing contact function

---
+++ src/components/util/csvToJSON.js
@@ -0,0 +1,14 @@
+export default function csvToJSON(file) {
+  const lines = file.split('\n');
+  const result = [];
+  const headers = lines[0].split(',');
+  for (let i = 1; i < lines.length; i++) {
+    const obj = {};
+    const currentLine = lines[i].split(',');
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentLine[j];
+    }
+    result.push(obj);
+  }
+  return JSON.stringify(result);
+}
